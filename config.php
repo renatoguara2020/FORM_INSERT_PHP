fd2f41c13dccc0d1e50f6e4b7189e31799bb8e81
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-05 14:27:18 -0300

    ok

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+//	Conexão	ao	banco	de	dados	(MySQL)
+define("BD_SERVIDOR",	"127.0.0.1");
+define("BD_USUARIO",	"sistematarefa");
+define("BD_SENHA",	"sistema");
+define("BD_BANCO",	"tarefas");
+
+
+?>
