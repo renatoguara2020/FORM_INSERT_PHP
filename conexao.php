373345f618cb0a37c1b0be29cff6333910899b31
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-05 12:22:01 -0300

    ok

--- conexao.php
+++ conexao.php
@@ -14,7 +14,7 @@
         //Conexão sem a porta
         $conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
         //echo "Conexão com banco de dados realizado com sucesso.";
-    } catch (PDOException $err) {
-        echo "Erro: Conexão com banco de dados não realizado com sucesso. Erro gerado " . $err->getMessage();
+    } catch (PDOException $e) {
+        echo "Erro: Conexão com banco de dados não realizado com sucesso. Erro gerado " . $e->getMessage();
     }
     //Fim da conexão com o banco de dados utilizando PDO
